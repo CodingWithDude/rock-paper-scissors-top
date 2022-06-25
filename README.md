2574d54215bf50c76c76f80f8cacadafb91f29a4
CodingWithDude <codingwithdude@gmail.com>
2022-06-25 16:12:02 -0700

    Added .pretterrc file which updated formatting

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rock-paper-scissors-top
-The Odin Project assignment of rock paper scissors
 
+The Odin Project assignment of rock paper scissors
 
 First project goal is to make a rock paper scissors game that you interact with via the console. Future
 project will update this with a GUI.
--- index.js
+++ index.js
@@ -12,50 +12,51 @@ Make player selecton case-insensitive
 Currently only output is via console - see readme for more info
 */
 
-const moveOptions = ["rock","paper","scissors"]
+const moveOptions = ["rock", "paper", "scissors"];
 
 // function for computer's move
 function computerMove() {
-    // random computerSelection of 0,1,2 for rock paper scissors
-    let randomChoice = Math.floor(Math.random()*3)
-    // return value for computerSelection
-    return moveOptions[randomChoice]
+  // random computerSelection of 0,1,2 for rock paper scissors
+  let randomChoice = Math.floor(Math.random() * 3);
+  // return value for computerSelection
+  return moveOptions[randomChoice];
 }
 
 // function for players move
 function playerMove() {
-    // get player input in lowercase string
-    let playerChoice = prompt("Please choose a move of rock, paper, or scissors").toLowerCase()
-    // validate input loop .indexOf returns -1 if no match in array
-    while (moveOptions.indexOf(playerChoice) === -1) {
-        alert("That is not a valid choice!")
-        playerChoice = prompt("Please choose a move of rock, paper, or scissors")
-    }
-    return playerChoice
+  // get player input in lowercase string
+  let playerChoice = prompt(
+    "Please choose a move of rock, paper, or scissors"
+  ).toLowerCase();
+  // validate input loop .indexOf returns -1 if no match in array
+  while (moveOptions.indexOf(playerChoice) === -1) {
+    alert("That is not a valid choice!");
+    playerChoice = prompt("Please choose a move of rock, paper, or scissors");
+  }
+  return playerChoice;
 }
 
 // function to playGame
-function playGame(){
-    // get computer and player moves
-    const playerSelection = playerMove()
-    console.log("Player Selection: " + playerSelection)
-    const computerSelection = computerMove()
-    console.log("Computer Selection: " + computerSelection)
-    
+function playGame() {
+  // get computer and player moves
+  const playerSelection = playerMove();
+  console.log("Player Selection: " + playerSelection);
+  const computerSelection = computerMove();
+  console.log("Computer Selection: " + computerSelection);
 
-    // check for tie or winner
-    if (computerSelection === playerSelection) {
-        console.log("The game is a Tie!")
-    } else if (computerSelection === "paper" && playerSelection === 'rock') {
-        console.log("The Computer Wins!")
-    } else if (computerSelection === "rock" && playerSelection === 'scissors') {
-        console.log("The Computer Wins!")
-    } else if (computerSelection === "scissors" && playerSelection === 'paper') {
-        console.log("The Computer Wins!")
-    } else {
-        console.log("The Player Wins!")
-    }
+  // check for tie or winner
+  if (computerSelection === playerSelection) {
+    console.log("The game is a Tie!");
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    console.log("The Computer Wins!");
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
+    console.log("The Computer Wins!");
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    console.log("The Computer Wins!");
+  } else {
+    console.log("The Player Wins!");
+  }
 }
 
 // runs game
-playGame()
+playGame();
